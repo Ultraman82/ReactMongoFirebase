5e944651c0d044047117bbea9a7352c4f95d3699
Changhyun Jeong <chjeong@e1z2r3p20.42.us.org>
2019-05-01 11:31:59 -0700

    React Mongo Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyAJnZr_xUWJ58tDmpb11N7FtPMWgykvIJI",
+  authDomain: "confusionserver-75420.firebaseapp.com",
+  databaseURL: "https://confusionserver-75420.firebaseio.com",
+  projectId: "confusionserver-75420",
+  storageBucket: "confusionserver-75420.appspot.com",
+  messagingSenderId: "136831272052"  
+};
